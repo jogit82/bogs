3632c2ed9041e87370a1391f843fa0f8497f1f49
jogit82 <jo.c.keller@gmail.com>
2016-01-04 02:31:22 -0800

    complete

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -4,20 +4,47 @@ class CreaturesController < ApplicationController
   end
 
   def create
+    # render plain: params[:creature].inspect
+    Creature.create creature_params
+    redirect_to creatures_path
   end
 
   def new
+    @creature = Creature.new
   end
 
   def edit
+    @creature = Creature.find params[:id]
   end
 
   def show
+    @creature = Creature.find params[:id]
+    if @creature.nil?
+      @creatures = Creature.all
+      flash.now[:alert] = "Your bog was not found"
+      render "index"
+    end
+    # render json: params
   end
 
   def update
+    @creature = Creature.find params[:id]
+    if @creature.update creature_params
+      redirect_to creatures_path
+    else 
+      render :edit
+    end
   end
 
   def destroy
+    Creature.find(params[:id]).delete #or destroy
+    # flash[:success] = "User deleted"
+    redirect_to creatures_path
+  end
+
+  private
+
+  def creature_params
+    params.require(:creature).permit(:name, :description)
   end
 end
